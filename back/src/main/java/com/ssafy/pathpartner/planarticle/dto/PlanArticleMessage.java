c6d4271d267f54dd86da0b567c20a3c6b3d0eb9e
boogahead <wherestuffgoes2@gmail.com>
2023-11-22 14:39:36 +0900

    dealt with concurrent update + ordering updates in simultaneous instances

--- back/src/main/java/com/ssafy/pathpartner/planarticle/dto/PlanArticleMessage.java
+++ back/src/main/java/com/ssafy/pathpartner/planarticle/dto/PlanArticleMessage.java
@@ -3,8 +3,11 @@ package com.ssafy.pathpartner.planarticle.dto;
 object that will be saved to redis so that clients will know what to do with the given object
  */
 public class PlanArticleMessage {
+
     private String operation;//create, update, delete
     private PlanArticleDto planArticleDto;//the object clients need to process
+
+    private long sequenceNumber;//the sequence number of the message
 
     public String getOperation() {
         return operation;
@@ -20,5 +23,13 @@ public class PlanArticleMessage {
 
     public void setPlanArticleDto(PlanArticleDto planArticleDto) {
         this.planArticleDto = planArticleDto;
+    }
+
+    public long getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(long sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
     }
 }
